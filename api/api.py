07bc5465133852013bd2c283d76644ce1fa4d091
Michael Bennett <mtbennett9010@gmail.com>
2022-01-27 10:26:03 +1300

    initial testing (first time I've used react and flask together)

--- api/api.py
+++ api/api.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+import time
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
