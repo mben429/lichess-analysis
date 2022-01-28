b5dd8be8f5011b16d9bf58eafb19f712e489b275
Michael Bennett <mtbennett9010@gmail.com>
2022-01-28 16:58:34 +1300

    basic form

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form'
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
-
-  useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
-  }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>The current time is {currentTime}.</p>
-      </header>
+      <Form />
     </div>
   );
 }
